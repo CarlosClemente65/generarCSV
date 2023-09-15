0a94334db050d6c8cbb5d2d48ade64255c56b25d
Carlos Clemente <carlos.clemente.abc@gmail.com>
2023-09-16 01:57:40 +0200

    Añadida funcionalidad para pasar como parametro la longitud de la cadena de caracteres

--- generarCSV/Program.cs
+++ generarCSV/Program.cs
@@ -1,12 +1,13 @@
 ﻿/*  Generador de codigos seguros de verificacion (CSV)
     Creado por Carlos Clemente - sep/2023
-    En cada ejecucion se genera una cadena de 8 caracteres de forma aleatoria y se le
-    añade al principio los dias transcurridos desde el 01/01/2000 y al final los segundos
-    del dia de hoy hasta el momento de la generacion.
-    La combinacion de los 8 caracteres (1,5 billones de combinaciones posibles) junto a la 
-    referencia de los dias y segundos, hace practicamente imposible que se repita el mismo numero,
-    ya que aunque se generasen varios en el mismo segundo (cosa que no es posible), se garantiza
-    la unicidad por la cadena de 8 caracteres aleatorios
+    En cada ejecucion se genera una cadena de entre 6 y 10 caracteres de forma aleatoria (por defecto se generan 8)
+    y se le añade al principio los dias transcurridos desde el 01/01/2000 y al final los segundos del dia de hoy
+    hasta el momento de la generacion; si se pasa como parametro un numero fuera del intervalo (6 o 10), la cadena se
+    generará con el defecto (8 caracteres)
+    Una cadena aleatoria de 8 caracteres genera unos 1,5 billones de combinaciones posibles, por lo que junto a la 
+    referencia de los dias y segundos, hace practicamente imposible que se repita el mismo numero, ya que aunque se
+    generasen varios en el mismo segundo (cosa que no es posible), se garantiza la unicidad por la cadena de
+    caracteres aleatorios.
 */
 
 using System;
@@ -18,27 +19,39 @@ namespace generarCSV
     internal class Program
     {
         //Definicion de variables
-        private static readonly string Caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private static readonly string Caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; //Caracteres que se utilizan en la generacion
         private static readonly Random Random = new Random();
+        private static int largoCadena = 8; //Sera el defecto de longitud de la cadena
 
-        static void Main(string[] args)
+        static void Main(string[] parametros)
         {
-            //Se genera el CSV con 8 caracteres
-            string codigoCSV = CSVGenerator(8);
-            //Console.WriteLine(codigoCSV);
-            //Console.ReadLine();
+            int num;
+            // Comprueba si se pasa como parametro el numero de caracteres a generar
+            if (parametros != null)
+            {
+                if (int.TryParse(parametros[0], out num))
+                {
+                    if (num >= 6 && num <= 10)
+                    {
+                        largoCadena = num;
+                    }
+                }
+            }
+
+            //Se genera el CSV con los caracteres pasados
+            string codigoCSV = CSVGenerator(largoCadena);
 
             //Guarda el resultado en un archivo
-            string nombreArchivo = "resultado.txt";
+            string nombreArchivo = "codigoCSV.txt";
             try
             {
                 using (StreamWriter grabar = new StreamWriter(nombreArchivo))
                 {
                     string texto = $"El codigo CSV generado es: {codigoCSV}";
                     grabar.WriteLine(texto);
-                } 
+                }
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 Console.WriteLine("Error al generar el archivo: " + ex.Message);
             }
@@ -46,11 +59,11 @@ namespace generarCSV
 
         public static string CSVGenerator(int longitud)
         {
-            // Generar cadena de 8 caracteres de forma aleatoria (son 1,5 billones de combinaciones posibles)
+            // Generar cadena de n caracteres de forma aleatoria
             StringBuilder csv = new StringBuilder();
             for (int i = 0; i < longitud; i++)
             {
-                // Elegir un carácter aleatorio de la cadena de caracteres
+                // Elegir un carácter aleatorio de la cadena de caracteres y añadirlo a la variable
                 char randomChar = Caracteres[Random.Next(Caracteres.Length)];
                 csv.Append(randomChar);
             }
@@ -63,7 +76,7 @@ namespace generarCSV
 
             //Calculo de los segundos transcurridos hoy en formato hexadecimal
             string hexSegundos = (DateTime.Now.Hour * 3600 + DateTime.Now.Minute * 60 + DateTime.Now.Second * 60).ToString("X5");
-            csvCalculado = hexDias + csvCalculado.ToString() + hexSegundos;
+            csvCalculado = hexDias + csvCalculado + hexSegundos;
 
             return csvCalculado;
         }
